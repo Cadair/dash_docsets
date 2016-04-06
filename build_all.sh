ba232c86690cbee46ef7eec8827a446d40107a42
Stuart Mumford <stuart@cadair.com>
2016-04-06 14:05:28 +0100

    fix upload, temp disable astropy

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./build_docset.sh sunpy http://docs.sunpy.org
-./build_docset.sh astropy http://docs.astropy.org
+#./build_docset.sh astropy http://docs.astropy.org
--- build_docset.sh
+++ build_docset.sh
@@ -8,4 +8,4 @@ cd $NAME
 python setup.py build_docs
 cd ..
 doc2dash -n $NAME -d output/ -j -u $WEB -v $NAME/build/sphinx/html
-tar --exclude='.DS_Store' -cvzf $NAME.tgz output/$NAME.docset
+tar --exclude='.DS_Store' -cvzf output/$NAME.tgz output/$NAME.docset
