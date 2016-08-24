3a77b6c1816ea7538e1b950a0d9015e5a63591c9
Stuart Mumford <stuart@cadair.com>
2016-08-24 15:06:55 +0100

    Add wcsaxes

--- build_all.sh
+++ build_all.sh
@@ -2,3 +2,4 @@
 
 ./build_docset.sh sunpy http://docs.sunpy.org https://raw.githubusercontent.com/sunpy/sunpy-logo/master/generated/sunpy_icon_64x64.png
 ./build_docset.sh astropy http://docs.astropy.org https://raw.githubusercontent.com/astropy/astropy-logo/master/generated/astropy_logo_notext.png
+./build_docset.sh wcsaxes http://wcsaxes.readthedocs.io https://raw.githubusercontent.com/astropy/astropy-logo/master/generated/astropy_logo_notext.png
